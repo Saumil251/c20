cbe2ff3fe87d7336cb6087bcd87691f185d04dc6
Saumil251 <72085873+Saumil251@users.noreply.github.com>
2020-10-18 13:59:10 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,31 @@
 var fixedRect
 var movingRect
+var object1
 
 function setup() {
   createCanvas(800,400);
   fixedRect=createSprite(400, 200, 50, 50);
   movingRect=createSprite(300,100,50,50);
+  object1=createSprite(10,200,50,50)
   fixedRect.shapeColor="red"
   movingRect.shapeColor="red"
-
+fixedRect.velocityX=-5
+object1.velocityX=5
 }
 
 function draw() {
   background(255,255,255); 
   movingRect.x=mouseX
   movingRect.y=mouseY
-  if(fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2 &&
-    movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2 && 
-    fixedRect.y-movingRect.y<movingRect.height/2+fixedRect.height/2 &&
-    movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2  ){
-fixedRect.shapeColor="yellow"
-movingRect.shapeColor="yellow"
+  bounceOff(fixedRect,object1)
+  if(isTouching(movingRect,object1)){
+movingRect.shapeColor="orange"
+object1.shapeColor="orange"
+ }
+else{
+  movingRect.shapeColor="lightGreen"
+object1.shapeColor="lightGreen"
+}
 
-    } 
-    else{
-      fixedRect.shapeColor="red"
-  movingRect.shapeColor="red"
-    }
   drawSprites();
 }
